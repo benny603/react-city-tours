e3d4f065832f6ec58ccbd1add7d0f437bb342266
Benny Wu <bennywu603@gmail.com>
2020-03-05 02:22:07 +0800

    add files to gh

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList/TourList";
+import './App.scss';
+
+function App() {
+  return (
+  <main>
+  <Navbar />
+  <TourList />
+  </main>
+  );
+}
+
+export default App;
